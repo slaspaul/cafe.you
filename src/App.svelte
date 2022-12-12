0ed50a81eb8350a6cc9a75375608fd9896d71584
slaspaul <slastenko667@gmail.com>
2022-12-12 23:14:10 +0300

    auth handler

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,24 @@
 <script>
 	export let name;
+
+	let count = 0;
+
+	function handleClick() {
+		count += 1;
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Как вкусно</a> ням ням ням.</p>
+	<h1>Здарова {name}!</h1>
+	<p> Как вкусно ням ням ням.</p>
+	<button on:click={handleClick}>
+		Clicked {count} {count === 1 ? 'time' : 'times'}
+	</button>
 </main>
 
 <style>
 	main {
-		text-align: center;
+		text-align: left;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
